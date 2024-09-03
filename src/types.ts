b46fd1dfe2626ef2840671e7a9c24d10e2e2c61a
cbcruk <cbcruk@gmail.com>
2024-09-03 15:08:33 +0900

    update: `FunctionComponentProps` 타입 추가

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,12 @@
 import { ConfigType, Dayjs } from 'dayjs'
+import { FC } from 'react'
 
 export type DateConfigType = {
   date: ConfigType
 }
 
 export type Parameter<T extends Dayjs[keyof Dayjs]> = Parameters<T>[0]
+
+export type FunctionComponentProps<T> = {
+  children: FC<T>
+}
