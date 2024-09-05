41c05f546ca02f4b67f6fddaa54abe29724f26c1
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-09-05 21:21:29 +0900

    fix: 속성값 주석 추가

--- src/types.ts
+++ src/types.ts
@@ -2,12 +2,14 @@ import { ConfigType, Dayjs } from 'dayjs'
 import { FC } from 'react'
 
 export type DateConfigType = {
+  /** 날짜 값 (문자열, Date 객체, Dayjs 객체 중 하나) */
   date: ConfigType
 }
 
 export type Parameter<T extends Dayjs[keyof Dayjs]> = Parameters<T>[0]
 
 export type FunctionComponentProps<T> = {
+  /** 렌더링할 함수 컴포넌트 */
   children: FC<T>
 }
 
