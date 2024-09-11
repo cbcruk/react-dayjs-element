23f650bb0b7170ee2812acafbb7424eff3a4b2e8
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-09-11 21:37:19 +0900

    fix: 대소문자 충돌 해결

--- src/UseDayjs.tsx
+++ src/UseDayjs.tsx
@@ -1,11 +1,22 @@
-import { useDayjs } from './useDayjs'
 import { DateConfigType, FunctionComponentProps } from './types'
+import dayjs from 'dayjs'
+import { useState } from 'react'
+import { isValidDate } from './utils'
 
 type Props = Partial<DateConfigType> &
-  FunctionComponentProps<ReturnType<typeof useDayjs>>
+  FunctionComponentProps<ReturnType<typeof useDayjsHook>>
+
+function useDayjsHook({ date }: DateConfigType) {
+  const [d] = useState(() => dayjs(date))
+
+  return {
+    d,
+    isValidDate: isValidDate(d),
+  }
+}
 
 export function UseDayjs({ date, children }: Props) {
-  const { d, isValidDate } = useDayjs({ date })
+  const { d, isValidDate } = useDayjsHook({ date })
 
   if (!isValidDate) {
     return null
