5aaad7791d0c39a75ed3aa102c42d147b8c8a906
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-09-11 22:30:43 +0900

    update: build 옵션 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime', 'dayjs'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'React-dom',
+          'react/jsx-runtime': 'react/jsx-runtime',
+          dayjs: 'dayjs',
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   test: {
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
